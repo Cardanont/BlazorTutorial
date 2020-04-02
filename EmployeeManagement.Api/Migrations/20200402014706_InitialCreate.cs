e00ac2000fc58dd3d404728ab564b7d5b91e2b22
Daniel Ontiveros <cardanont@gmail.com>
2020-04-01 23:46:32 -0600

    Migrations on the DB for Employees and actualization of .net core tools

---
+++ EmployeeManagement.Api/Migrations/20200402014706_InitialCreate.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagement.Api.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmentName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.DepartmentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    DateOfBirth = table.Column<DateTime>(nullable: false),
+                    Gender = table.Column<int>(nullable: false),
+                    DepartmentId = table.Column<int>(nullable: false),
+                    PhotoPath = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                    table.CheckConstraint("CK_Employees_Gender_Enum_Constraint", "[Gender] IN(0, 1, 2)");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Departments",
+                columns: new[] { "DepartmentId", "DepartmentName" },
+                values: new object[,]
+                {
+                    { 1, "IT" },
+                    { 2, "HR" },
+                    { 3, "Payroll" },
+                    { 4, "Admin" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "EmployeeId", "DateOfBirth", "DepartmentId", "Email", "FirstName", "Gender", "LastName", "PhotoPath" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1980, 10, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), 3, "JohnH@pragmthech.com", "John", 0, "Hastings", "images/john.png" },
+                    { 2, new DateTime(1988, 7, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, "MaryM@pragmthech.com", "Mary", 1, "Martin", "images/mary.png" },
+                    { 3, new DateTime(1995, 2, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), 4, "KendraP@pragmthech.com", "Kendra", 1, "Pothard", "images/kendra.png" },
+                    { 4, new DateTime(1986, 2, 8, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, "CarlosD@pragmthech.com", "Carlos", 0, "Devops", "images/carlos.png" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
