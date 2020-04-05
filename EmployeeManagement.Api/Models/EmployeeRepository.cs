db88e874e95045a8e85211a2e14ad88274522446
Daniel Ontiveros <cardanont@gmail.com>
2020-04-04 23:53:45 -0600

    Class EmplyeeRepo and DI

---
+++ EmployeeManagement.Api/Models/EmployeeRepository.cs
@@ -0,0 +1,44 @@
+﻿using EmployeeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Api.Models
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppDbContext appDbContext;
+
+
+        public EmployeeRepository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public Task<Employee> AddEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteEmployee(int employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> GetEmployee(int employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Employee>> GetEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> UpdateEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
