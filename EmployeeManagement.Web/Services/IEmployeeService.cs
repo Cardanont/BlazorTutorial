7e935368e5eec3c50ed5efd3f1513cb14f510a0d
Daniel Ontiveros <cardanont@gmail.com>
2020-04-22 00:01:43 -0500

    Remember this foolish mistake dude! https

--- EmployeeManagement.Web/Services/IEmployeeService.cs
+++ EmployeeManagement.Web/Services/IEmployeeService.cs
@@ -8,8 +8,6 @@ namespace EmployeeManagement.Web.Services
 {
     public interface IEmployeeService
     {
-
         Task<IEnumerable<Employee>> GetEmployees();
-
     }
 }
--- EmployeeManagement.Web/Startup.cs
+++ EmployeeManagement.Web/Startup.cs
@@ -30,7 +30,7 @@ namespace EmployeeManagement.Web
             services.AddServerSideBlazor();
             services.AddHttpClient<IEmployeeService, EmployeeService>(client =>
             {
-                client.BaseAddress = new Uri("http://localhost:44313");
+                client.BaseAddress = new Uri("https://localhost:44313");
             });
         }
 
