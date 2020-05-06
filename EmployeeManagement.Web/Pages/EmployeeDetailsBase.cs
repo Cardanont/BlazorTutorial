8546fbc323a0863a3296a140c40fe5f217e1322e
Daniel Ontiveros <cardanont@gmail.com>
2020-05-05 23:00:54 -0500

    Hide footer event example

--- EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
@@ -15,6 +15,9 @@ namespace EmployeeManagement.Web.Pages
 
         protected string Coordinates { get; set; }
 
+        protected string ButtonText { get; set; } = "Hide Footer";
+        protected string CssClass { get; set; } = null;
+
         [Inject]
         public IEmployeeService EmployeeService { get; set; }
 
@@ -27,9 +30,18 @@ namespace EmployeeManagement.Web.Pages
             Employee = await EmployeeService.GetEmployee(int.Parse(Id));
         }
 
-        //protected void Mouse_Move(MouseEventArgs e)
-        //{
-        //    Coordinates = $"X = {e.ClientX} Y = {e.ClientY}";
-        //}
+        protected void Button_Click()
+        {
+            if(ButtonText == "Hide Footer")
+            {
+                ButtonText = "Show Footer";
+                CssClass = "HideFooter";
+            }
+            else
+            {
+                CssClass = null;
+                ButtonText = "Hide Footer";
+            }
+        }
     }
 }
