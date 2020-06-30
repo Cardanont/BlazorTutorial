e8538854acb52b53b3778f76e113f0507c9615c6
Daniel Ontiveros <cardanont@gmail.com>
2020-06-29 23:58:34 -0500

    Typo on domain

--- EmployeeManagement.Models/CustomValidators/EmailDomainValidator.cs
+++ EmployeeManagement.Models/CustomValidators/EmailDomainValidator.cs
@@ -10,10 +10,10 @@ namespace EmployeeManagement.Models.CustomValidators
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             string[] strings = value.ToString().Split('@');
-            if (strings[1].ToUpper() == "PRAGMTECH.COM")
+            if (strings[1].ToUpper() == "PRAGIMTECH.COM")
                 return null;
 
-            return new ValidationResult("Domain must be PragmTech.com",
+            return new ValidationResult("Domain must be PragimTech.com",
                 new[] { validationContext.MemberName });
         }
     }
