99921f2511408bf5976d00c4d688ecd8e2b16a47
Daniel Ontiveros <cardanont@gmail.com>
2020-06-29 23:57:58 -0500

    Custom validator for email domain

---
+++ EmployeeManagement.Models/CustomValidators/EmailDomainValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EmployeeManagement.Models.CustomValidators
+{
+    class EmailDomainValidator : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string[] strings = value.ToString().Split('@');
+            if (strings[1].ToUpper() == "PRAGMTECH.COM")
+                return null;
+
+            return new ValidationResult("Domain must be PragmTech.com",
+                new[] { validationContext.MemberName });
+        }
+    }
+}
